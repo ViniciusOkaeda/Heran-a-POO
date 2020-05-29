ee61c56b7955c8917eca645c1f8447f4a11abe1a
Vinicius <viniciusokaeda@gmail.com>
2020-05-29 15:44:21 -0300

    estrutura finalizada

---
+++ Desktop/PrimeiroPOO/Aula6-Heranca/CNPJ.cs
@@ -0,0 +1,15 @@
+namespace Aula6_Heranca
+{
+    public class CNPJ : Pessoa
+    {
+        public string cnpj;
+
+
+        public bool ValidarCNPJ(){
+            if(cnpj != null){
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Desktop/PrimeiroPOO/Aula6-Heranca/CPF.cs
@@ -0,0 +1,18 @@
+namespace Aula6_Heranca
+{
+    public class CPF : Pessoa
+    {
+        public string cpf;
+
+        public string rg;
+
+
+        public bool ValidarCPF(){
+            if(cpf != null){
+                return true;
+            }
+                return false;
+            
+        }
+    }
+}
